d4c03f530281fb87fd1956cf7eb9b6c0670f1f49
Joyce Liu <krispyokreme@users.noreply.github.com>
2015-04-27 12:28:59 -0700

    Update modifiedMontyHall.js

--- modifiedMontyHall.js
+++ modifiedMontyHall.js
@@ -1,24 +1,49 @@
 var doors = ['red','blue','green'];
 var windoor = doors[Math.floor(Math.random() * 3)]; //chooses the winning door
 
-var user = prompt("Behind one of these doors is a goat that spits out diamonds everyday. Behind each of the two other doors is a broken down worthless car. Which of these doors do you choose ('red', 'blue', or 'green')?").toLowerCase();
+var notvalid = function () { //this function is called whenever the player types an invalid response to any of the prompts
+    alert("That was not a valid response. Please try again.");
+};
 
-var secondchoice = 'none';
-var drawcard = prompt("We'd like to improve your chances of winning by offering you a random draw from this deck of cards. If you draw a card with the diamond suit, then you get to choose a second door to open in addition to your first. If the goat is behind either one, you win! However, if you choose to take a chance on this draw and you end up winning the goat, then regardless of what you draw, you must agree to donate the goat's first year's worth of diamonds to a charity of your choice before the goat can belong to you. Would you like to take that chance and draw a card? ('yes' or 'no')").toLowerCase();
+for (var user = ''; doors.indexOf(user) === -1;) { //will keep reprompting until player enters valid response
+    if (user != '') {
+        notvalid();
+    };
+    user = prompt("Behind one of these doors is a goat that spits out diamonds everyday. Behind each of the two other doors is a broken down worthless car. Which of these doors do you choose ('red', 'blue', or 'green')?").toLowerCase();
+};
+
+var secondchoice = '';
+
+for (var drawcard = ''; ["yes","no"].indexOf(drawcard) === -1;) { //will keep reprompting until player enters valid response
+    if (drawcard != '') {
+        notvalid();
+    };
+    drawcard = prompt("We'd like to improve your chances of winning by offering you a random draw from this deck of cards. If you draw a card with the diamond suit, then you get to choose a second door to open in addition to your first. If the goat is behind either one, you win! However, if you choose to take a chance on this draw and you end up winning the goat, then regardless of what you draw, you must agree to donate the goat's first year's worth of diamonds to a charity of your choice before the goat can belong to you. Would you like to take that chance and draw a card? ('yes' or 'no')").toLowerCase();
+};
+
 var luckycard = false;
 if (drawcard === 'yes') {
     var suit = ['heart','spade','club','diamond'][Math.floor(Math.random() * 4)];
     luckycard = (suit === 'diamond');
     if (luckycard) {
-        secondchoice = prompt("You got a diamond card! Now you can choose a second door that you would like to open in addition to your first door ('red', 'blue', or 'green')?").toLowerCase();
+        while (doors.indexOf(secondchoice) === -1) { //will keep reprompting until player enters valid response
+            if (secondchoice != '') {
+                notvalid();
+            };
+            secondchoice = prompt("You got a diamond card! Now you can choose a second door that you would like to open in addition to your first door ('red', 'blue', or 'green')?").toLowerCase();
+        };
     }
     else {
         alert("Darn! You got a " + suit + ", not a lucky diamond. Don't forget, if you win the goat, you're still obligated to donate the goat's first year's worth of diamonds to a charity of your choice.");
     };
 };
 
-
-var cheating = prompt("Now you must answer honestly. Did you cheat by peeking behind the doors beforehand? ('yes' or 'no')").toLowerCase();
+for (var cheating = ''; ["yes","no"].indexOf(cheating) === -1;) { //will keep reprompting until player enters valid response
+    if (cheating != '') {
+        notvalid();
+    };
+    cheating = prompt("Now you must answer honestly. Did you cheat by peeking behind the doors beforehand? ('yes' or 'no')").toLowerCase();
+};
 
 var car = function () {
     var cars = ["Toyota Camry", "Honda Odyssey minivan", "Mini Cooper", "Ford Explorer", "Jeep Wrangler", "Chevrolet Sonic"];
@@ -39,7 +64,12 @@ switch(true) {
                 break;
             };
         };
-        var stayorswitch = prompt("Now, before we reveal to you whether you chose the right door, we'll let you know that at least you didn't choose the " + losedoor + " door, which would give you this old, scratched-up " + car() + " with its tires stolen and its bumpers falling off. Thank goodness you didn't choose that one! But that means the diamond-spitting goat is behind one of the other two doors. If you're having doubts about your choice, we will now give you the option to switch. Would you like to stay with the " + user + " door or switch to the " + switchdoor + " door? (type 'stay' or 'switch')").toLowerCase();
+        for (var stayorswitch = ''; ["stay","switch"].indexOf(stayorswitch) === -1;) { //will keep reprompting until player enters valid response
+            if (stayorswitch != '') {
+                notvalid();
+            };
+            stayorswitch = prompt("Now, before we reveal to you whether you chose the right door, we'll let you know that at least you didn't choose the " + losedoor + " door, which would give you this old, scratched-up " + car() + " with its tires stolen and its bumpers falling off. Thank goodness you didn't choose that one! But that means the diamond-spitting goat is behind one of the other two doors. If you're having doubts about your choice, we will now give you the option to switch. Would you like to stay with the " + user + " door or switch to the " + switchdoor + " door? (type 'stay' or 'switch')").toLowerCase();
+        };
         if (stayorswitch === "switch") {
             user = switchdoor;
         };
